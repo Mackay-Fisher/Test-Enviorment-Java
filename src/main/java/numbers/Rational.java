e55494dfc40409b6ad01ab6fc43f179926880c5c
Mackay-Fisher <Mackeyfisher@gmail.com>
2023-11-18 12:49:03 -0600

    initial commit

---
+++ src/main/java/numbers/Rational.java
@@ -0,0 +1,13 @@
+package numbers;
+
+/**
+ * Hello world!
+ *
+ */
+public class Rational 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
