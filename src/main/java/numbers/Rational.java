0951441b20119e89d6c80e4df864645f997915f0
Mackay-Fisher <Mackeyfisher@gmail.com>
2023-11-18 18:21:17 -0600

    not sure if I even need to add tehse methods but somethign is failing in my tests so I added new ones regardless

--- src/main/java/numbers/Rational.java
+++ src/main/java/numbers/Rational.java
@@ -9,10 +9,80 @@ public class Rational
     private int numerator;
     private int denominator;
 
-
     Rational(int a, int b){
-        this.numerator = a;
-        this.denominator = b;
+        boolean a_negative = a < 0;
+        boolean b_negative = b < 0;
+        int min = 0;
+        if (a_negative) {
+            a = -a;
+        }
+        if (b_negative) {
+            b = -b;
+        }
+        if (a == 0) {
+            this.numerator = 0;
+            this.denominator = 1;
+        } else {
+            //find gcd of a and b
+            int gcd = 1;
+            if(a<b){
+                min = a;
+            } else {
+                min = b;
+            }
+            for (int i = 1; i <=min; i++) {
+                if (a % i == 0 && b % i == 0) {
+                    gcd = i;
+                }
+            }
+            if(a_negative || b_negative){
+                a = -a;
+            }
+            a = a / gcd;
+            b = b / gcd;
+            this.numerator = a;
+            this.denominator = b;
+        }
+    }
+
+    Rational(Rational r){
+        this(r.numerator, r.denominator);
+    }
+
+    public Rational opposite(){
+        return new Rational(-this.numerator, this.denominator);
+    }
+
+    public Rational reciprocal(){
+        //check to make sure numerator is not 0
+        if(this.numerator == 0){
+            throw new IllegalArgumentException("Cannot divide by 0");
+        }
+        return new Rational(this.denominator, this.numerator);
+    }
+
+    public Rational times(Rational r){
+        return new Rational(this.numerator * r.numerator, this.denominator * r.denominator);
+    }
+
+    public Rational dividedBy(Rational r){
+        return this.times(r.reciprocal());
+    }
+
+    public Rational plus(Rational r){
+        return new Rational(this.numerator * r.denominator + r.numerator * this.denominator, this.denominator * r.denominator);
+    }
+
+    public Rational minus(Rational r){
+        return this.plus(r.opposite());
+    }
+
+    public Rational(int a) {
+        this(a, 1);
+    }
+
+    public Rational() {
+        this(0, 1);
     }
 
     public int numerator() {
--- src/test/java/numbers/RationalTest.java
+++ src/test/java/numbers/RationalTest.java
@@ -1,6 +1,9 @@
 package numbers;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThrows;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -36,11 +39,55 @@ public class RationalTest
      */
     public void testApp()
     {
-        // Given no preconditions
-        Rational value = new Rational(2, 3);
+        Rational test2 = new Rational(2);
         // Then the value should have numerator `2`
-        assertThat("the numerator should be 2", value.numerator(), is(2));
+        assertThat("the numerator should be 2", test2.numerator(), is(2));
+        // And the value should have denominator `1`
+        assertThat("the denominator should be 1", test2.denominator(), is(1));
+        
+        Rational test1 = new Rational();
+
+        assertThat("the numerator should be 0", test1.numerator(), is(0));
+        // And the value should have denominator `1`
+        assertThat("the denominator should be 1", test1.denominator(), is(1));
+        // Given no preconditions
+        Rational value = new Rational(48, -72);
+        // Then the value should have numerator `-2`
+        assertThat("48 / -72 = -2 / 3", value.numerator(), is(-2));
         // And the value should have denominator `3`
-        assertThat("the denominator should be 3", value.denominator(), is(3));
+        assertThat("48 / -72 = -2 / 3", value.denominator(), is(3));
+
+        Rational original = new Rational(2, 3);
+// When I create a `Rational` value as a copy of the original `Rational` value
+        Rational value2 = new Rational(original);
+        // Then the copy's value should have numerator 2
+        assertThat("the numerator should be 2", value2.numerator(), is(2));
+        // And the copy's value should have denominator 3
+        assertThat("the denominator should be 3", value2.denominator(), is(3));
+
+        Rational value3 = new Rational(2, 3);
+        Rational opposite = value3.opposite();
+        assertThat("the opposite of 2 is -2", opposite.numerator(), is(-2));
+        assertThat("the denominator should be 3", opposite.denominator(), is(3));
+
+
+        Rational value4 = new Rational(2, 3);
+        Rational reciprocal = value4.reciprocal();
+        assertThat("the numerator should be 3", reciprocal.numerator(), is(3));
+        assertThat("the denominator should be 2", reciprocal.denominator(), is(2));
+
+        Rational value5 = new Rational(0);
+        assertThrows(IllegalArgumentException.class, value5::reciprocal);
+
+        Rational p = new Rational(2, 3);
+        Rational q = new Rational(5, 7);
+        Rational result = p.times(q);
+        assertThat("2 * 5 = 10", result.numerator(), is(10));
+        assertThat("3 * 7 = 21", result.denominator(), is(21));
+
+
+
+        //Add more rigirous testings here
+
     }
 }
